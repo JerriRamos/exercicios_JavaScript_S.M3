fb7c03c32446cd2da4486a54a30f4041d5a6e106
jerryramos <jerryramos1995@gmail.com>
2020-06-11 00:54:48 -0100

    fix code add while

--- Exer_Ficha/exer_9.js
+++ Exer_Ficha/exer_9.js
@@ -1,13 +1,14 @@
-let numero = [1, 2, 3, 4, 5];
-let count = 1;
+let numero = 0;
+let count = 0;
 
-while (count <= 1) {
-  for (var i = 0; i < numero.length; i++) {
-    if (numero[i] % 2 == 0) {
-      console.log(numero[i], "Par");
+do {
+  for (var i = 1; i <= 1; i++) {
+    if (numero % 2 == 0) {
+      console.log(numero, "Par");
     } else {
-      console.log(numero[i], "Impar");
+      console.log(numero, "Impar");
     }
   }
+  numero++;
   count++;
-}
+} while (count <= 50);
